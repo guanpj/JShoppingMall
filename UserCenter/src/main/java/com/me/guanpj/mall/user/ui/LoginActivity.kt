fe5a5844e7c9f87926e44465fac45ea7f23dd9df
guanpj <guanpj.me@gmail.com>
2018-08-07 18:06:56 +0800

    init environment

--- UserCenter/src/main/java/com/me/guanpj/mall/user/ui/LoginActivity.kt
+++ UserCenter/src/main/java/com/me/guanpj/mall/user/ui/LoginActivity.kt
@@ -1,6 +1,14 @@
 package com.me.guanpj.mall.user.ui
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textview: TextView = TextView(this)
+        textview.setText("ldjflds")
+        setContentView(textview)
+    }
 }
