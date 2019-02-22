a95d83c9cb9917a96ff8babd3d682b930c2bb933
guanpj <guanpj.me@gmail.com>
2019-02-22 13:05:13 +0800

    hhh

--- BaseLibrary/src/main/java/com/me/guanpj/mall/library/core/BaseConstant.kt
+++ BaseLibrary/src/main/java/com/me/guanpj/mall/library/core/BaseConstant.kt
@@ -5,7 +5,8 @@ class BaseConstant{
         //七牛服务地址
         const val IMAGE_SERVER_ADDRESS = "http://osea2fxp7.bkt.clouddn.com/"
         //本地服务器地址
-        const val SERVER_ADDRESS = "http://192.168.1.111:8080"
+        //const val SERVER_ADDRESS = "http://192.168.1.111:8080"
+        const val SERVER_ADDRESS = "http://www.guanpj.cn:8080/Kotlin_Server/"
         //SP表名
         const val TABLE_PREFS = "Shopping_Mall"
         //Token Key
